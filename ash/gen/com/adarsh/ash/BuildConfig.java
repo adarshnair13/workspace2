92fa8180d290b925088629d8c7de5b64f00856dc
adarsh <adarshnair444@gmail.com>
2015-02-13 15:12:53 +0530

    weekly update 13.02.2015

---
+++ ash/gen/com/adarsh/ash/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.adarsh.ash;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ example25/gen/com/adarsh/example25/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.adarsh.example25;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
